8d9cfd8d38185ff3a82a3545522e3fe6010674c9
Meliser <mitya.korneev@mail.ru>
2019-04-07 19:15:50 +0300

    Added all logic for encryption

--- DES/main.cpp
+++ DES/main.cpp
@@ -6,7 +6,32 @@
 using namespace std;
 
 const size_t keySize = 64;
-
+  char IP[64] = { 58,	50,	42,	34,	26,	18,	10,	2,
+							60,	52,	44,	36,	28,	20,	12,	4,
+							62,	54,	46,	38,	30,	22,	14,	6,
+							64,	56,	48,	40,	32,	24,	16,	8,
+							57,	49,	41,	33,	25,	17,	9,	1,
+							59,	51,	43,	35,	27,	19,	11,	3,
+							61,	53,	45,	37,	29,	21,	13,	5,
+							63,	55,	47,	39,	31,	23,	15,	7 };
+  char IP_1[64] = { 40,	8,	48,	16,	56,	24,	64,	32,
+					39,	7,	47,	15,	55,	23,	63,	31,
+					38,	6,	46,	14,	54,	22,	62,	30,
+					37,	5,	45,	13,	53,	21,	61,	29,
+					36,	4,	44,	12,	52,	20,	60,	28,
+					35,	3,	43,	11,	51,	19,	59,	27,
+					34,	2,	42,	10,	50,	18,	58,	26,
+					33,	1,	41,	9,	49,	17,	57,	25 };
+  char P[48] = {		32, 1, 2, 3, 4, 5, 4, 5,
+							6, 7, 8, 9, 8, 9, 10, 11,
+							12, 13, 12, 13, 14, 15, 16, 17,
+							16, 17, 18, 19, 20, 21, 20, 21,
+							22, 23, 24, 25, 24, 25, 26, 27,
+							28, 29, 28, 29, 30, 31, 32, 1 };
+  char function1PBox[32] = {16, 7, 20, 21, 29, 12, 28, 17,
+							1, 15, 23, 26, 5, 18, 31, 10,
+							2, 8, 24, 14, 32, 27, 3, 9,
+							19, 13, 30, 6, 22, 11, 4, 25 };
 template<size_t bitsSize>
 bitset<bitsSize> reverse(bitset<bitsSize> &bits) {
 	bitset<bitsSize> reversed_bits;
@@ -97,15 +122,25 @@ char s8[64] = { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
 				1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
 				7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
 				2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 };
+char* s[8] = { s1,s2,s3,s4,s5,s6,s7,s8 };
 
-char* s[2] = { s2,s3 };
-class DesEncryption {
-public:
-	DesEncryption(unsigned long long key) : key_(key){}
-	
-private:
-	bitset<keySize> key_;
-};
+bitset<32> function1(bitset<32> &rPart, const bitset<48> &roundKey) {
+	bitset<48> expanded = rearrange<32, 48>(rPart, P);
+	expanded ^= roundKey;
+	bitset<6> currentVector;
+	unsigned row,column,offset = 6,offset1 = 28;
+	bitset<32> afterSBoxes;
+	for (size_t i = 0; i < 8; i++)
+	{
+		row = expanded[0+offset*i] * 2 + expanded[5+offset*i];
+		column = expanded[1 + offset * i] * 8 + expanded[2 + offset * i] * 4 + expanded[3 + offset * i] * 2 + expanded[4 + offset * i];
+		//cout << int(s[i][row * 16 + column]) << endl;
+		afterSBoxes |= (s[i][row * 16 + column]<< offset1);
+		offset1 -= 4;
+	}
+	afterSBoxes = reverse(afterSBoxes);
+	return rearrange<32, 32>(afterSBoxes, function1PBox);
+}
 
 class RoundKeyGenerator {
 public:
@@ -150,6 +185,9 @@ public:
 			cout << endl;
 		}
 	}
+	const bitset<48>& getRoundKey(size_t index)const {
+		return roundKeys[index];
+	}
 private:
 	 static char roundKeyTable_[56];
 	 static char finalTable_[48];
@@ -182,38 +220,70 @@ char RoundKeyGenerator::finalTable_[48] = { 14, 17, 11, 24, 1, 5, 3, 28,
 											41, 52, 31, 37, 47, 55, 30, 40,
 											51, 45, 33, 48, 44, 49, 39, 56,
 											34, 53, 46, 42, 50, 36, 29, 32 };
+
+class DesEncryption {
+public:
+	DesEncryption(unsigned long long key) : key_(key) {
+		roundKeyGenerator = new RoundKeyGenerator;
+		bitset<64> temp = reverse(key_);
+		roundKeyGenerator->initializeC0D0(temp);
+		roundKeyGenerator->generateRoundKeys(16);
+	}
+	~DesEncryption()
+	{
+		delete roundKeyGenerator;
+	}
+	void encrypt(bitset<64> &plainText) {
+
+		bitset<64>plainText_ = rearrange<64, 64>(plainText, IP);
+		for (size_t i = 0; i < 32; i++)
+		{
+			lPart[i] = plainText_[i];
+			rPart[i] = plainText_[i + 32];
+		}
+		bitset<32> temp;
+		for (size_t i = 0; i < 16; i++)
+		{
+			temp = lPart;
+			lPart = rPart;
+			rPart = (temp^function1(rPart, roundKeyGenerator->getRoundKey(i)));
+
+		}
+		bitset<64> encrypted;
+		for (size_t i = 0; i < 32; i++)
+		{
+			encrypted[i] = rPart[i];
+			encrypted[i+32] = lPart[i];
+		}
+		encrypted = rearrange<64, 64>(encrypted, IP_1);
+	}
+private:
+	bitset<keySize> key_;
+	RoundKeyGenerator *roundKeyGenerator;
+	bitset<32> lPart;
+	bitset<32> rPart;
+
+};
 int main() {
 	//setlocale(0, "rus");
 	{
 		Timer timer(__FUNCTION__);
 		try {
 			const unsigned keyLength = 64;
-			const char IP[64] = { 58,	50,	42,	34,	26,	18,	10,	2,
-							60,	52,	44,	36,	28,	20,	12,	4,
-							62,	54,	46,	38,	30,	22,	14,	6,
-							64,	56,	48,	40,	32,	24,	16,	8,
-							57,	49,	41,	33,	25,	17,	9,	1,
-							59,	51,	43,	35,	27,	19,	11,	3,
-							61,	53,	45,	37,	29,	21,	13,	5,
-							63,	55,	47,	39,	31,	23,	15,	7 };
+		
+			
 
-			const char P[48] = { 32, 1, 2, 3, 4, 5, 4, 5,
-							6, 7, 8, 9, 8, 9, 10, 11,
-							12, 13, 12, 13, 14, 15, 16, 17,
-							16, 17, 18, 19, 20, 21, 20, 21,
-							22, 23, 24, 25, 24, 25, 26, 27,
-							28, 29, 28, 29, 30, 31, 32, 1 };
-			cout << int(s[0][2])<<endl;
-			RoundKeyGenerator k;
-			bitset<64> new_key(0xAABB09182736CCDD);
-			bitset<64> key = reverse(new_key);//!
-			k.initializeC0D0(key);
-			k.generateRoundKeys(16);
-			//bitset<64> raw_data(0x123456ABCD132536);//!
-			//left_cycle_shift(raw_data,2);
-			//right_cycle_shift(raw_data, 2);
-			//bitset<64> before_IP = reverse<64>(raw_data);
-			//bitset<64> after_IP = rearrange<64,64>(before_IP,IP);
+			DesEncryption d(0xAABB09182736CCDD);
+			bitset<64> plain(0x123456ABCD132536);
+			bitset<64> plainReversed = reverse(plain);
+			d.encrypt(plainReversed);
+
+			//RoundKeyGenerator k;
+			//bitset<64> new_key(0xAABB09182736CCDD);
+			//bitset<64> key = reverse(new_key);//!
+			//k.initializeC0D0(key);
+			//k.generateRoundKeys(16);
+			
 			
 			
 		}
